55bd3299361556e261efd49b76ef5a7ecd9acf7d
Veronica Medeiros <veronicakmedeiros@gmail.com>
2024-04-11 21:29:57 -0300

    refatoração - parte 3 - inclusão de testes do Entities

---
+++ backend/src/test/groovy/org/linketinder/Entities/VacancyTest.groovy
@@ -0,0 +1,23 @@
+package org.linketinder.Entities;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VacancyTest {
+
+    @Test
+    void createVacancyTest() {
+
+        //Given
+        Vacancy vacancyTest = new Vacancy("Engenheiro de Dados", "Pleno", "Tarde", "Presencial", "Campinas", "São Paulo",
+                "Elabora relatórios gerenciais e prevê tendências de mercado. Responsável pelo desenvolvimento e manutenção de aplicativos web e móveis.",
+                [4, 7, 8], 44)
+
+        //When
+        boolean vacancyExists = vacancyTest
+
+        //Than
+        assertTrue(vacancyExists)
+    }
+}
