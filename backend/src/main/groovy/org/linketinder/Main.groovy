edfecb4b361042d7f3d697286dab5a22b9995b31
Veronica Medeiros <veronicakmedeiros@gmail.com>
2024-04-30 10:06:55 -0300

    refatoração

--- backend/src/main/groovy/org/linketinder/Main.groovy
+++ backend/src/main/groovy/org/linketinder/Main.groovy
@@ -1,12 +1,10 @@
 package org.linketinder
 
-import org.linketinder.Initial
 
 class Main {
      static void main(String[] args) {
 
          Initial startApp = new Initial()
          startApp.execute()
-
      }
 }
